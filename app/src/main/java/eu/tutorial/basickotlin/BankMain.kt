e75f9279c6f58e1473edb1bb39b7c0076fb69f03
Andreas Zelios <a.zelios@satways.net>
2026-01-17 14:43:15 +0200

    used Bankaccount class in main

--- app/src/main/java/eu/tutorial/basickotlin/BankMain.kt
+++ app/src/main/java/eu/tutorial/basickotlin/BankMain.kt
@@ -1,5 +1,14 @@
 package eu.tutorial.basickotlin
 
 fun main(){
-   println("something")
+   val kostasAccount = BankAccount("Kostas", 100.0)
+   val adonisAccount = BankAccount("Adonis", 120.0)
+
+   kostasAccount.deposit(20.0)
+   kostasAccount.displayTransactionHistory()
+
+   adonisAccount.transferMoney(kostasAccount, 10.0)
+   adonisAccount.displayTransactionHistory()
+   kostasAccount.displayTransactionHistory()
+
 }
